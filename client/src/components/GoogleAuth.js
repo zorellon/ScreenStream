333052fac3bb83b71838d76952f1909debc45d6c
Jean-Philippe McCluskey <jeanphilippe.mccluskey@gmail.com>
2019-03-02 17:41:40 -0500

    setting up OAuthentication with google for app sign in/out

---
+++ client/src/components/GoogleAuth.js
@@ -0,0 +1,60 @@
+import React from 'react';
+
+class GoogleAuth extends React.Component {
+    state = { isSignedIn: null };
+
+    componentDidMount(){
+        window.gapi.load('client:auth2', () => {
+            window.gapi.client.init({
+                clientId: '34522299756-n57jo942g1pnoqpn6qve8dtnfh1e34h7.apps.googleusercontent.com',
+                scope: 'email'
+            }).then(() => {
+                this.auth = window.gapi.auth2.getAuthInstance();
+                this.setState({ isSignedIn: this.auth.isSignedIn.get()});
+                this.auth.isSignedIn.listen(this.onAuthChange);
+            });
+        });
+    }
+
+    onAuthChange = () => {
+        this.setState({isSignedIn: this.auth.isSignedIn.get()});
+    };
+
+    onSignIn = () => {
+        this.auth.signIn();
+    };
+
+    onSignOut = () => {
+        this.auth.signOut();
+    };
+
+    renderAuthButton() {
+        if(this.state.isSignedIn === null){
+            return null;
+        } else if(this.state.isSignedIn === true){
+            return (
+                <button onClick={this.onSignOut} className="ui red google button">
+                    <i className="google icon"/>
+                    Logout
+                </button>
+            );
+        }else{
+            return (
+                <button onClick={this.onSignIn} className="ui red google button">
+                    <i className="google icon"/>
+                    Sign In
+                </button>
+            );
+        }
+    }
+
+    render() {
+        return (
+            <div>
+                {this.renderAuthButton()}
+            </div>
+        );
+    };
+};
+
+export default GoogleAuth;
--- client/src/components/Header.js
+++ client/src/components/Header.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import {Link} from 'react-router-dom';
+import GoogleAuth from './GoogleAuth';
 
 const Header = () => {
     return(
@@ -11,6 +12,8 @@ const Header = () => {
             <Link to="/" className ="item">
                 Stream List
             </Link>
+            <GoogleAuth/>
+            
             </div>
         </div>
     );
