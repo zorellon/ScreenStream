a7c31cd771a5720a00f98ad4adb01cc11cc52b73
Jean-Philippe McCluskey <jeanphilippe.mccluskey@gmail.com>
2019-03-03 13:29:09 -0500

    installing redux dev tools extention for state debbuging

--- client/src/index.js
+++ client/src/index.js
@@ -2,12 +2,17 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 
 import {Provider} from 'react-redux';
-import {createStore} from 'redux';
+import {createStore, applyMiddleware} from 'redux';
+import { composeWithDevTools } from 'redux-devtools-extension';
 
 import App from './components/App';
 import reducers from './reducers';
 
-const store = createStore(reducers);
+
+const store = createStore(
+    reducers,
+    composeWithDevTools(applyMiddleware())
+);
 
 ReactDOM.render(
     <Provider store = {store}>
