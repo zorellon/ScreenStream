c551358cf4bfcea45b2ef3b79bd6739eb305d57b
Jean-Philippe McCluskey <jeanphilippe.mccluskey@gmail.com>
2019-03-03 12:07:50 -0500

    refactor authentication using redux

--- client/src/index.js
+++ client/src/index.js
@@ -1,15 +1,17 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 
-//import {Provider} from 'react-redux';
-//import {createStore} from 'redux';
+import {Provider} from 'react-redux';
+import {createStore} from 'redux';
 
 import App from './components/App';
-//import reducers from './reducers';
+import reducers from './reducers';
+
+const store = createStore(reducers);
 
 ReactDOM.render(
-    //<Provider store = {createStore(reducers)}>
-        <App />,
-   // </Provider>, 
+    <Provider store = {store}>
+        <App />
+   </Provider>, 
     document.querySelector('#root')
 );
