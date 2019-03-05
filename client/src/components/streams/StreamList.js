bbebf1b2dad4fddb7a01df3a8ff526dc29e5de55
Jean-Philippe McCluskey <jeanphilippe.mccluskey@gmail.com>
2019-03-05 12:56:14 -0500

    adding create stream navigation button

--- client/src/components/streams/StreamList.js
+++ client/src/components/streams/StreamList.js
@@ -1,8 +1,10 @@
 import React from 'react';
 import {connect} from 'react-redux';
+import {Link} from 'react-router-dom';
 import {fetchStreams} from '../../actions';
 
 class StreamList extends React.Component {
+
     componentDidMount() {
         this.props.fetchStreams();
     }
@@ -18,37 +20,43 @@ class StreamList extends React.Component {
                         Delete
                     </button>
                 </div>
-            )
-        }
+            );
+        };
     }
 
     renderList(){
         return this.props.streams.map(stream => {
             return(
                 <div className="item" key = {stream.id}>
-                    {this.renderAdminButtons(stream)}
                     <i className = "large middle aligned icon camera"/>
                     <div className="content">
                         {stream.title}
-                
-                    <div className="description"> 
-                    {stream.description}
+                        <div className="description">{stream.description}</div>
                     </div>
-                    </div>
+                    
                 </div>
             );
         });
     }
 
-
+    renderCreateButton(){
+        if (this.props.isSignedIn) {
+            return (
+                <div style = {{textAlign: 'right'}}>
+                <Link to="/streams/new" className="ui button primary">Create Stream</Link>
+                </div>
+            );
+        }
+    }
 
     render() {
         return(
             <div>
-            <h2>Stream List:</h2>
-            <div className= "ui celled list">
-                {this.renderList()}
-            </div>
+                <h2>Stream List:</h2>
+                <div className= "ui celled list">
+                    {this.renderList()}
+                </div>
+                {this.renderCreateButton()}
             </div>
         );
     }
