31fccb8cf00798f5dc59c5bf992d28fd1ea2bd31
Jean-Philippe McCluskey <jeanphilippe.mccluskey@gmail.com>
2019-03-04 20:42:20 -0500

    creating action creators for all actions following REST conventions

--- client/src/components/streams/StreamCreate.js
+++ client/src/components/streams/StreamCreate.js
@@ -1,5 +1,7 @@
 import React from 'react';
 import {Field,reduxForm} from 'redux-form';
+import {connect} from 'react-redux';
+import {createStream} from '../../actions';
 
 
 class StreamCreate extends React.Component {
@@ -31,8 +33,9 @@ class StreamCreate extends React.Component {
             // />
     }
 
-    onSubmit(formValues){
-        console.log(formValues);
+    onSubmit = (formValues) => {
+        //console.log(formValues);
+        this.props.StreamCreate(formValues);
     }
 
     render(){
@@ -72,7 +75,13 @@ const validate = (formValues) => {
     return errors;
 };
 
-export default reduxForm({
+const formWrapped = reduxForm({
     form: 'streamCreate',
     validate
 })(StreamCreate);
+
+export default connect(
+    //mapStateToProps, 
+    null,
+    {createStream}
+)(formWrapped);
--- client/src/index.js
+++ client/src/index.js
@@ -4,14 +4,16 @@ import ReactDOM from 'react-dom';
 import {Provider} from 'react-redux';
 import {createStore, applyMiddleware} from 'redux';
 import { composeWithDevTools } from 'redux-devtools-extension';
+import reduxThunk from 'redux-thunk';
 
 import App from './components/App';
 import reducers from './reducers';
 
 
+
 const store = createStore(
     reducers,
-    composeWithDevTools(applyMiddleware())
+    composeWithDevTools(applyMiddleware(reduxThunk))
 );
 
 ReactDOM.render(
