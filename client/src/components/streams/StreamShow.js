198c4295bd977cc2a20068ba46c28f8fc8636d1a
Jean-Philippe McCluskey <jeanphilippe.mccluskey@gmail.com>
2019-03-07 18:01:16 -0500

    Implementation of streaming video with Open Broadcast Soft in StreamShow component

--- client/src/components/streams/StreamShow.js
+++ client/src/components/streams/StreamShow.js
@@ -1,19 +1,49 @@
 import React from 'react';
+import flv from 'flv.js';
 import {connect} from 'react-redux';
 import {fetchStream} from '../../actions';
 
 class StreamShow extends React.Component {
 
+    constructor(props){
+        super(props);
+
+        this.videoRef = React.createRef();
+    }
+
     componentDidMount() {
         this.props.fetchStream(this.props.match.params.id);
+        this.buildPlayer();
+    }
+
+    componentDidUpdate() {
+        this.buildPlayer();
+    }
+    componentWillUnmount() {
+        // console.log('I was unmounted');
+        this.player.destroy();
+    }
+
+    buildPlayer(){
+        if (this.player || !this.props.stream){
+            return;
+        }
+
+        this.player = flv.createPlayer({
+            type: 'flv',
+            url: `http://localhost:8000/live/${this.props.match.params.id}.flv`
+        });
+        this.player.attachMediaElement(this.videoRef.current);
+        this.player.load();
     }
 
     render(){
         if (!this.props.stream){
-            return <div>Loading...</div>
+            return <div>Loading...</div>;
         }
         return(
             <div>
+                <video ref={this.videoRef} style={{width: '100%'}} controls={true}/>
                 <h1>{this.props.stream.title}</h1>
                 <h5>{this.props.stream.description}</h5>
             </div>
