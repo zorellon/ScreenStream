0bfbc31c6c5b9bf7115fb3313712763970057746
Jean-Philippe McCluskey <jeanphilippe.mccluskey@gmail.com>
2019-03-07 15:21:35 -0500

    setting up StreamShow component

--- client/src/components/streams/StreamList.js
+++ client/src/components/streams/StreamList.js
@@ -31,10 +31,9 @@ class StreamList extends React.Component {
                     {this.renderAdminButtons(stream)}
                     <i className = "large middle aligned icon camera"/>
                     <div className="content">
-                        {stream.title}
+                        <Link to={`/streams/${stream.id}`}>{stream.title}</Link>
                         <div className="description">{stream.description}</div>
-                    </div>
-                    
+                    </div>             
                 </div>
             );
         });
--- client/src/components/streams/StreamShow.js
+++ client/src/components/streams/StreamShow.js
@@ -1,12 +1,33 @@
 import React from 'react';
+import {connect} from 'react-redux';
+import {fetchStream} from '../../actions';
 
+class StreamShow extends React.Component {
 
-const StreamShow = () => {
-    return(
-        <div>
-            StreamShow
-        </div>
-    );
+    componentDidMount() {
+        this.props.fetchStream(this.props.match.params.id);
+    }
+
+    render(){
+        if (!this.props.stream){
+            return <div>Loading...</div>
+        }
+        return(
+            <div>
+                <h1>{this.props.stream.title}</h1>
+                <h5>{this.props.stream.description}</h5>
+            </div>
+        );
+    }
+
 };
 
-export default StreamShow;
+const mapStateToProps = (state, ownProps) => {
+    //console.log(ownProps);
+    return{ stream: state.streams[ownProps.match.params.id]};
+};
+
+export default connect(
+    mapStateToProps,
+     {fetchStream}
+)(StreamShow);
